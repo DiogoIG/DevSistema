87fda85aa08a4ab78692e1536675f2597fc105e9
DiogoIG <Diogocalixtoaguiar@gmail.com>
2026-06-16 11:34:30 -0300

    AULA11

--- UC1/AULA_11/vetorarraylista.js
+++ UC1/AULA_11/vetorarraylista.js
@@ -1,1 +1,22 @@
 console.log("mensagem ")
+
+
+
+let nomes= [
+    "João Paulo",
+    "Maria Luíza",
+    "Carlos José",
+    "Pedro Lúcio"
+];
+
+let tamanho = nomes.length;
+
+console.log(`Tamanho da Array: ${tamanho} `)
+console.log(`Exibindo dados da array`)
+console.log(`Posição 1 ${nomes[1]}`)
+
+let cont = 1
+
+ for(cont; cont <= 10; cont++){
+    console.log(`${cont}. ${nomes[cont]}  `)
+ }
